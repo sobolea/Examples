647f3280f44b43d95c2cdac340eda7b394d4dc19
67anna <@DESKTOP-VICLL7Q.(none)>
2023-03-10 23:43:40 +0300

    add example005_ifelse project

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите своё имя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "lisa")
+{
+    Console.WriteLine("Любимая привет!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
